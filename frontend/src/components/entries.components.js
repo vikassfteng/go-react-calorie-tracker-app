5f46897f9724eefcc8a5a5d524b969f6c5fe4c10
vikassfteng <vikas.sft.engg.1@gmail.com>
2024-03-30 01:10:28 +0530

    Adding frontend using react boostrap

---
+++ frontend/src/components/entries.components.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from 'react';
+
+import axios from 'axios';
+
+import {Button, Form, Container, Modal} from 'react-bootstrap';
+
+import Entry from './single-entry.components';
---
+++ frontend/src/components/single-entry.components.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+import {Button, Card, Row, Col} from 'react-bootstrap';
+
+const Entry =({entryData, setChangeIngredient, deleteSingleEntry, setChangeEntry}) => {
+    return(
+        <Card>
+            <Row>  
+                <Col> Dish:{entryData !== undefined && entryData.dish}</Col>
+                <Col> Ingredients:{entryData !== undefined && entryData.ingredients}</Col>
+                <Col> Calories:{entryData !== undefined && entryData.calories}</Col>
+                <Col> Fat:{entryData !== undefined && entryData.fat}</Col>
+
+                <col><Button onclick={()=> deleteSingleEntry(entryData._id)}>delete Entry </Button></col>
+                <col><Button onclick={()=> ChangeIngredient()}>change ingredients </Button></col>
+                <col><Button onclick={()=> ChangeEntry()}>change entry </Button></col>
+                
+            </Row>
+        </Card>
+    )
+
+    function ChangeIngredient(){
+        setChangeIngredient(
+            {
+                "change": true,
+                "id": entryData._id
+            }
+        )
+    }
+    function ChangeEntry(){
+        setChangeEntry(
+            {
+                "change": true,
+                "id": entryData._id
+            }
+        )
+    }
+}
